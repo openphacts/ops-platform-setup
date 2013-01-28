645b27d317f8ece154b4a3c59d2025b74736e615
AlasdairGray <s08s5Eas>
2013-01-28 12:47:00 +0000

    Extract ChEBI version from OWL file

--- scripts/chebi/generateVoID.sh
+++ scripts/chebi/generateVoID.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+CMDS="roqet awk tr"
+ 
+for i in $CMDS
+do
+        # command -v will return >0 when the $i is not found
+	command -v $i >/dev/null && continue || { echo "$i command not found and required by script."; exit 1; }
+done
+
 RETRIEVED_FILE="chebiRetrieved.owl"
 VOID_TEMPLATE="chebi_void.ttl.in"
 VOID_OUTPUT_BASE="chebi_void"
@@ -15,7 +23,10 @@ SCRIPT_RUNNER="https://orcid.org/0000-0002-5711-4872"
 # %%SCRIPT_RUNTIME%%
 # %%SCRIPT_RUNNER%%
 
+CHEBI_VERSION_QUERY="chebi_version_query.rq"
+
 ##TODO: Extract vocabulary list from owl file!
+##TODO: Link to previous version (probably needs to be a parameter)
 
 function retrieve_file()
 {
@@ -29,16 +40,32 @@ function retrieve_file()
     fi
 }
 
+function run_query()
+{
+    query=$1
+    output=$2
+    roqet -r json -D $RETRIEVED_FILE $query > $output
+}
+
+function extract_CHEBI_Version() 
+{
+    query="SELECT ?version WHERE { <http://purl.obolibrary.org/obo> <http://www.w3.org/2002/07/owl#versionIRI> ?version }"
+    run_query $CHEBI_VERSION_QUERY "version.out"
+    CHEBI_VERSION=`awk -F"[,:]" '{for(i=1;i<=NF;i++){if($i~/value\042/){print $(i+1)}}}' "version.out" | tr -d ' ' | tr -d '"'`
+    rm "version.out"
+}
+
 function extract_parameters()
 {
-    CHEBI_VERSION=1
+    extract_CHEBI_Version
+    #CHEBI_VERSION=1
     CHEBI_DATETIME="2013-01-25T00:00:00Z"
     CHEBI_DATADUMP=$1 
 }
 
 function write_void()
 {
-    echo $CHEBI_VERSION
+    echo "ChEBI Version: $CHEBI_VERSION"
     echo $CHEBI_DATETIME
     echo $CHEBI_DATADUMP
     echo $SCRIPT_RUNNER
