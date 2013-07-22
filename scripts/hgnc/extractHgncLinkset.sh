035f9b689081362d71b1cf0c5c74ca55660444cf
AlasdairGray <A.Gray@cs.man.ac.uk>
2013-07-22 15:04:54 +0100

    Started HGNC linkset script
    Using Andra's commands from https://openphacts2011.atlassian.net/browse/IMS-302

---
+++ scripts/hgnc/extractHgncLinkset.sh
@@ -0,0 +1,4 @@
+OUTPUT_FILE=/tmp/hgncLinksets.ttl
+
+curl "http://www.genenames.org/cgi-bin/hgnc_downloads?col=gd_hgnc_id&col=gd_app_sym&status=Approved&status_opt=2&where=&order_by=gd_hgnc_id&format=text&limit=&hgnc_dbtag=on&submit=submit" |cut -f2 -d:| awk '{print "<http://identifiers.org/hgnc/"$1 "> <http://www.w3.org/2004/02/skos/core#exactMatch> <http://identifiers.org/hgnc.symbol/"$2">" 
+'} > $OUTPUT_FILE 
