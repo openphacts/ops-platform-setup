3d27385b9cf581ee122d37416238726be652c299
fundatureanu-sever <fundatureanu.sever@gmail.com>
2014-02-10 09:50:32 +0100

    Dependency fix for the LDA

--- vagrant_install/bootstrap.sh
+++ vagrant_install/bootstrap.sh
@@ -3,7 +3,7 @@
 rm /var/lib/apt/lists/* -vf
 apt-get update
 apt-get install -y apache2
-apt-get install -y php5 memcached php5-memcached php5-memcache
+apt-get install -y php5 memcached php5-memcached php5-memcache php5-curl
 a2enmod rewrite
 sudo service apache2 restart
 
@@ -12,14 +12,15 @@ apt-get install -y git
 #Install Linked Data API
 sudo mkdir /var/www
 cd /var/www
-sudo git clone https://github.com/openphacts/OPS_LinkedDataApi -b develop html
+sudo git clone https://github.com/openphacts/OPS_LinkedDataApi -b meta_calls html
 sudo sed -i 's,/var/www,/var/www/html,' /etc/apache2/sites-available/default
 cat /etc/apache2/sites-available/default | tr "\n" "|" | sed 's,\(<Directory /var/www/html/>[^<]*\)AllowOverride None\([^<]*</Directory>\),\1AllowOverride All\2,' | sed 's/|/\n/g' >~/temp
 sudo mv ~/temp /etc/apache2/sites-available/default
 sudo mkdir /var/www/html/logs
-sudo chmod 766 /var/www/html/logs
+sudo chmod 777 /var/www/html/logs
 sudo mkdir /var/www/html/cache
-sudo chmod 766 /var/www/html/cache
+sudo chmod 777 /var/www/html/cache
+sudo chown -R vagrant:vagrant /var/www/html
 sudo service apache2 restart
 
 #Install IMS
