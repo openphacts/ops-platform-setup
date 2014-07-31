723fd7cf03141b8b1951516bb774ec63f6b21f67
Antonis Loizou <a.loizou@vu.nl>
2014-07-31 13:41:47 +0200

    Updated to 1.4 and removed unused script

---
+++ scripts/precacheEverything.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+time ./precacheTreeFromFile.sh enzyme json beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh enzyme tsv beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh chebi json beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh chebi tsv beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh chembl json beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh chembl tsv beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh go json beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
+time ./precacheTreeFromFile.sh go tsv beta.openphacts.org/1.4 be7ce5a9 77069eed83fceb8bb23f031c05193151
