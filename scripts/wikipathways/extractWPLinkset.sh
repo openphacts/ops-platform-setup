9c0c6c5ff864f7a454927a4fd9af9e8dc9ba55ed
Stian Soiland-Reyes <stain@apache.org>
2017-05-03 17:10:51 +0100

    Use wp:pathwayOntologyTag property

--- scripts/wikipathways/extractWPLinkset.sh
+++ scripts/wikipathways/extractWPLinkset.sh
@@ -23,7 +23,7 @@ write_void() {
 extract_links() {
     echo "@prefix void:    <http://rdfs.org/ns/void#> ." > $OUTPUT_LINKSET_FILE
     echo "@prefix wprdf: <http://rdf.wikipathways.org/> ." >> $OUTPUT_LINKSET_FILE
-    curl "http://sparql.wikipathways.org/?default-graph-uri=&query=CONSTRUCT+%7B%3FwpIdentifier+skos%3ArelatedMatch+%3FpathwayOntology+.%7D+%0D%0AWHERE+%7B%0D%0A++%3Fpathway+wp%3ApathwayOntology+%3FpathwayOntology+.%0D%0A++%3Fpathway+dc%3Aidentifier+%3FwpIdentifier+.%0D%0A++FILTER+regex%28%3FpathwayOntology%2C+%22PW_%22%2C+%22i%22%29+%0D%0A%7D&format=text%2Frdf%2Bn3&timeout=0&debug=on" >> $OUTPUT_LINKSET_FILE
+    curl "http://sparql.wikipathways.org/?default-graph-uri=&query=CONSTRUCT+%7B%3FwpIdentifier+skos%3ArelatedMatch+%3FpathwayOntology+.%7D+%0D%0AWHERE+%7B%0D%0A++%3Fpathway+wp%3ApathwayOntologyTag+%3FpathwayOntology+.%0D%0A++%3Fpathway+dc%3Aidentifier+%3FwpIdentifier+.%0D%0A++FILTER+regex%28%3FpathwayOntology%2C+%22PW_%22%2C+%22i%22%29+%0D%0A%7D&format=text%2Frdf%2Bn3&timeout=0&debug=on" >> $OUTPUT_LINKSET_FILE
     echo >> $OUTPUT_LINKSET_FILE
     echo "$BASE_URI void:inDataset wprdf:wp-poLinkset" >> $OUTPUT_LINKSET_FILE
 }
